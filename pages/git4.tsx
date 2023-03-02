2e851d32b30ece2c43420acb809963c8e6ffc3bb
Kengo Kobayashi <kken91914@gmail.com>
2023-03-02 17:35:01 +0900

    pages/git4.tsx,git5.tsxの2ファイルを作成

---
+++ pages/git4.tsx
@@ -0,0 +1,7 @@
+export default function Git4() {
+  return (
+    <>
+      <h1>Git4</h1>
+    </>
+  );
+}
---
+++ pages/git5.tsx
@@ -0,0 +1,7 @@
+export default function Git5() {
+  return (
+    <>
+      <h1>Git5</h1>
+    </>
+  );
+}
